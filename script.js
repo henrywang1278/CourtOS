36e91dd9a86933911d2651b8e94b3b8eeadc526b
henrywang1278 <henrywang1278@gmail.com>
2026-09-20 17:57:49 -0400

    Initialize MediaPipe Pose Landmarker

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+import {
+  FilesetResolver,
+  PoseLandmarker,
+  DrawingUtils
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/+esm";
+
+let poseLandmarker = null;
+
+async function createPoseLandmarker() {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+  );
+
+  poseLandmarker = await PoseLandmarker.createFromOptions(
+    vision,
+    {
+      baseOptions: {
+        modelAssetPath:
+          "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task",
+        delegate: "GPU"
+      },
+
+      runningMode: "VIDEO",
+      numPoses: 1
+    }
+  );
+
+  console.log("Pose Landmarker loaded!");
+}
+
+createPoseLandmarker();
 const uploadButton = document.getElementById("uploadButton");
 const videoInput = document.getElementById("videoInput");
 const videoPreview = document.getElementById("videoPreview");
